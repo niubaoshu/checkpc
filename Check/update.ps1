13e92d87b8872328f52d2e5e082a3875122bc885
niubaoshu <niubaoshu@gmail.com>
2024-12-13 19:06:14 -0800

    all

--- Check/update.ps1
+++ Check/update.ps1
@@ -1,24 +1,18 @@
 function AutoUpdate {
-    # GitHub 仓库信息
-    $owner = "niubaoshu"  # 替换为仓库的所有者（用户名或组织名）
-    $repo = "checkpc"    # 替换为仓库的名称
+    $owner = "niubaoshu"  
+    $repo = "checkpc"    
 
     # GitHub API URL
     $apiUrl = "https://api.github.com/repos/$owner/$repo/releases/latest"
 
     $updated = $false
-    # 获取最新发布信息
     try {
         $releaseInfo = Invoke-RestMethod -Uri $apiUrl -Method Get
-
-        # 提取最新版本号
         $latestVersion = $releaseInfo.tag_name
-
         $outputPath = $parentDirectory + "\checkpc_${latestVersion}.zip"
         $extractPath = $parentDirectory + "\checkpc_${latestVersion}"
         Write-Host $latestVersion $Version
         if ($latestVersion -ne $Version) {
-            #Invoke-WebRequest -Uri $releaseInfo.assets[0].browser_download_url -OutFile $outputPath
             Invoke-WebRequest -Uri $releaseInfo.zipball_url -OutFile $outputPath
             if (Test-Path -Path $outputPath) {
                 Expand-Archive -Path $outputPath -DestinationPath $extractPath
